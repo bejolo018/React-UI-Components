a5ed08ef844c8398d907d920181bbad33b46e05e
unknown <bejolo018@gmail.com>
2019-04-22 15:52:37 -0500

    completed all components and begun groundwork for styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () =>{
+    return <div>
+        <p>this is the cards content aosdnoasin</p>
+    </div>
+}
+
+export default CardContent
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return(
+        <div class='headertitle'>
+            <h1>Lambda School</h1>
+            <p> @lambdaschool 4/22/2019 </p>
+        </div>
+    );
+};
+
+export default HeaderTitle;
