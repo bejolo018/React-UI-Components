e7e5916964463283978e623ba854b924ddc01f6e
unknown <bejolo018@gmail.com>
2019-04-23 14:39:40 -0500

    completed calculatordisplay.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,40 @@
 import React from 'react';
 import './Button.css';
+
+// const NumberButton = [
+//     {
+//         number:'1'
+//     },
+//     {
+//         number:'2'
+//     },
+//     {
+//         number: '3'
+//     },
+//     {
+//         number:'4'
+//     },
+//     {
+//         number:'5'
+//     },
+//     {
+//         number:'6'
+//     },
+//     {
+//         number:'7'
+//     },
+//     {
+//         number:'8'
+//     },
+//     {
+//         number:'9'
+//     },
+// ];
+
+// function NumberButtons(){
+//     return(
+//         <div>
+//             <h1>Number 1</h1>
+//         </div>
+//     )
+// }
